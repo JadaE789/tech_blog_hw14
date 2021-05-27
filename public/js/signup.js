97c33df5ce201dc8e878d641777e9839d3f3e428
Jada Desormeaux <JadaE789@gmail.com>
2021-05-26 23:14:40 -0400

    models and public

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,19 +2,14 @@ async function signupFormHandler(event) {
     event.preventDefault();
   
     const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-    const twitter = document.querySelector('#twitter-signup').value.trim();
-    const github = document.querySelector('#github-signup').value.trim();
   
-    if (username && email && password) {
+  
+    if (username && password) {
       const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
           username,
-          email,
-          twitter,
-          github,
           password
         }),
         headers: { 'Content-Type': 'application/json' }
